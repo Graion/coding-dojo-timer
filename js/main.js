aebfba32a7bfb3a7f7b95ca962a2f52d332369a3
Santiago Rojo <tiagox@gmail.com>
2016-01-20 18:08:26 -0300

    Convert `time` to an object instead of an array

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@
     }
 
     function display() {
-        var el = clock.querySelector('.display')
-        ;(display = function () {
-            el.textContent = pad(time[0]) + ':' + pad(time[1])
+        var el = clock.querySelector('.display');
+
+        (display = function () {
+            el.textContent = pad(time.minutes) + ':' + pad(time.seconds);
         })()
     }
 
-    var clock = document.querySelector('.clock')
-      , stage = document.body
-      , beep = clock.querySelector('audio')
-      , time, tick, timeout
+    var clock = document.querySelector('.clock'),
+        stage = document.body,
+        beep = clock.querySelector('audio'),
+        time,
+        tick,
+        timeout;
 
     var action = {
         reset: function () {
@@ -23,7 +26,7 @@
             stage.classList.remove('timeout')
             timeout = false
             beep.currentTime && (beep.currentTime = 0)
-            time = [5, 0]
+            time = {minutes: 7, seconds: 0}
             display()
         }
       , pause: function (audio) {
@@ -40,19 +43,19 @@
     }
 
     function ticker() {
-        time[1]--
-        if (time[1] < 0) {
-            time[1] = 59
-            time[0]--
+        time.seconds--;
+        if (time.seconds < 0) {
+            time.seconds = 59;
+            time.minutes--;
         }
-        if (!time[0]) {
-            if (parseInt(beep.duration, 10) >= time[1]) {
+        if (!time.minutes) {
+            if (parseInt(beep.duration, 10) >= time.seconds) {
                 beep.play()
             }
-            if (!time[1]) {
+            if (!time.seconds) {
                 stage.classList.add('timeout')
-                timeout = true
-                action.pause(true)
+                timeout = true;
+                action.pause(true);
             }
         }
         display()
